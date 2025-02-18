05ae64b992312734e2df7e744fe366ac2fc794e3
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-02-18 23:36:12 +0300

    Initial commit

---
+++ src/main/java/ru/sema1ary/sema1ary_website/Sema1aryWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ru.sema1ary.sema1ary_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sema1aryWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sema1aryWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sema1ary/sema1ary_website/Sema1aryWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sema1ary.sema1ary_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sema1aryWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
